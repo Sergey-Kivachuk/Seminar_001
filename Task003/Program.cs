42dbe1d76e4eadccbae4952405a753ff1228ae8e
Sergey Kivachuk <you@example.com>
2023-04-09 16:19:19 +0300

    изменил программу в  Task003

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -3,34 +3,38 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.WriteLine ("ВВедите два числа ");
+Console.WriteLine ("Введите день недели ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number == 1) 
 { 
     Console.WriteLine ("понедельник");
 }
 
-if (number == 2) 
+else if (number == 2) 
 { 
     Console.WriteLine ("вторник");
 }
-if (number == 3) 
+else if (number == 3) 
 { 
     Console.WriteLine ("среда");
 }
-if (number == 4) 
+else if (number == 4) 
 { 
     Console.WriteLine ("четверг");
 }
-if (number == 5) 
+else if (number == 5) 
 { 
     Console.WriteLine ("пятница");
 }
-if (number == 6) 
+else if (number == 6) 
 { 
     Console.WriteLine ("суббота");
 }
-if (number == 7) 
+else if (number == 7) 
 { 
     Console.WriteLine ("воскресенье");
+}
+else
+{ 
+    Console.WriteLine ("нет такого дня недели");
 }
